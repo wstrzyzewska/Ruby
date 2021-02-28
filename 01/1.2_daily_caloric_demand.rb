ccbba059fc70702aa625912506e1c8348004b4be
Weronika Strzyżewska <54662444+wstrzyzewska@users.noreply.github.com>
2021-02-28 17:21:43 +0100

    Update 1.2_daily_caloric_demand.rb

--- 01/1.2_daily_caloric_demand.rb
+++ 01/1.2_daily_caloric_demand.rb
@@ -5,12 +5,12 @@ age1 = 25
 height1 = 170
 weight1 = 63
 
-DCD = (10 * weight1 + 6.25 * height1 - 5 * age1 + s_female) * 1.6
-print "Your DCD is: #{DCD}"
+dcd = (10 * weight1 + 6.25 * height1 - 5 * age1 + s_female) * 1.6
+print "Your DCD is: #{dcd}"
 
 age2 = 26
 height2 = 175
 weight2 = 59
 
-MyDCD = (10 * weight2 + 6.25 * height2 - 5 * age2 + s_female) * 1.2
-print "My DCD is: #{DCD}"
+my_dcd = (10 * weight2 + 6.25 * height2 - 5 * age2 + s_female) * 1.2
+print "My DCD is: #{my_dcd}"
