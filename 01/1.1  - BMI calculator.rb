f37f8b3a29ea25e70756afcadd259428cc4ef65e
Weronika Strzyżewska <54662444+wstrzyzewska@users.noreply.github.com>
2021-02-28 17:20:40 +0100

    Update and rename 1.1  - BMI calculator.rb to 1.1_bmi_calculator.rb

--- 01/1.1  - BMI calculator.rb
+++
@@ -1,4 +0,0 @@
-height = 1.60
-weight = 53
-BMI = weight / (height ** 2)
-print "Your bmi is: #{BMI}"
---
+++ 01/1.1_bmi_calculator.rb
@@ -0,0 +1,4 @@
+height = 1.60
+weight = 53
+bmi = weight / (height ** 2)
+print "Your bmi is: #{bmi}"
