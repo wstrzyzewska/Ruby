5ec045ecca0334a4521c75e5b886373d70b0c2cd
Weronika Strzyżewska <54662444+wstrzyzewska@users.noreply.github.com>
2021-02-28 15:42:28 +0100

    Create 2.1 - Harry Potter.rb

---
+++ 01/2.1 - Harry Potter.rb
@@ -0,0 +1,12 @@
+puts "What's your name?"
+name = gets
+
+puts "Hello #{name}"
+
+puts "What's your age?"
+age = gets
+
+puts "What station does traing to Hogwarts leave from?"
+station = gets
+
+print ("In your age #{age} there is no sense in coming to station number #{station}")
