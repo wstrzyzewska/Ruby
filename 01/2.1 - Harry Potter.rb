cf1b48471a448f67cab2f6859b01bc1021382905
Weronika Strzyżewska <54662444+wstrzyzewska@users.noreply.github.com>
2021-02-28 16:22:04 +0100

    Update 2.1 - Harry Potter.rb

--- 01/2.1 - Harry Potter.rb
+++ 01/2.1 - Harry Potter.rb
@@ -1,12 +1,12 @@
 puts "What's your name?"
-name = gets
+name = gets.chomp
 
 puts "Hello #{name}"
 
 puts "What's your age?"
-age = gets
+age = gets.chomp
 
 puts "What station does traing to Hogwarts leave from?"
-station = gets
+station = gets.chomp
 
 print ("In your age #{age} there is no sense in coming to station number #{station}")
