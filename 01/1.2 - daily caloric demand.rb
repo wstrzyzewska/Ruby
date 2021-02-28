ecd843170100fef60ce445846705f4b043fcbeb1
Weronika Strzyżewska <54662444+wstrzyzewska@users.noreply.github.com>
2021-02-28 12:57:40 +0100

    Create 1.2 - daily caloric demand.rb

---
+++ 01/1.2 - daily caloric demand.rb
@@ -0,0 +1,16 @@
+s_female = -161
+s_male = 5
+
+age1 = 25
+height1 = 170
+weight1 = 63
+
+DCD = (10 * weight1 + 6.25 * height1 - 5 * age1 + s_female) * 1.6
+print "Your DCD is: #{DCD}"
+
+age2 = 27
+height2 = 175
+weight2 = 59
+
+MyDCD = (10 * weight2 + 6.25 * height2 - 5 * age2 + s_female) * 1.2
+print "My DCD is: #{DCD}"
