f03a6eb3e1c0f7143cb26457fd9db6dac9055b53
Weronika Strzyżewska <54662444+wstrzyzewska@users.noreply.github.com>
2021-03-01 20:33:56 +0100

    Update 3_1_units.rb

--- 01/3_1_units.rb
+++ 01/3_1_units.rb
@@ -1,9 +1,9 @@
 puts "Length in cm: "
 cm = gets.chomp
 cm_to_int = cm.to_i
-p "Lenght in meters: #{"%.3f" % (cm_to_int * 0.01)}, lenght in inches: #{"%.3f" % (cm_to_int * 0.393701)}."
+puts "Lenght in meters: #{"%.3f" % (cm_to_int * 0.01)}, lenght in inches: #{"%.3f" % (cm_to_int * 0.393701)}."
 
 puts "Weigth in kg: "
 kg = gets.chomp
 kg_to_int = kg.to_i
-p "Weight in pounds: #{"%.3f" % (kg_to_int * 2.20462)}."
+puts "Weight in pounds: #{"%.3f" % (kg_to_int * 2.20462)}."
